8cb383544950cbcb33784d5930b5b5e4cbbbb96a
TTRoyal <78960032+TTRoyal@users.noreply.github.com>
2021-02-12 16:33:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,14 @@
 //const prefix = config.prefix
 module.exports = {
     prefix: `.`,
-   
+    status1: 'La phobie d\'un bot c\'est de se perdre entre les lignes de code.',
+    status2:'La pation d\'un bot c\'est de pouvoir être bien au chaud dans un serveur.',
+    status3:'A quoi je sert?',
+    status4:'Ma vie est très courte seulement 3h.',
+    status5:'Vous lisez ça?',
+    status6:'Mon créateur est TT Royal.',
+    status7:'tu es grave sympa',
+    status8:'Pourquoi les humains n\'ont pas de bug?',
     activity: `Mon prefix est. ` ,
     MsgConsole: `Je suis prêt`
 }
