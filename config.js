a456ac14be120475542429410f474627d0b1c59c
TTRoyal <78960032+TTRoyal@users.noreply.github.com>
2021-02-12 12:38:06 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-//const config = require('./config.js')
-//const prefix = config.prefix
-module.exports = {
-    prefix: `.`,
-    //token: `NzY3MzE0OTYxNzIxOTgyOTk3.X4wHvg.BvfzYXCYAixvrqV4nZBJrEMjJvE`,
-    activity: `Mon prefix est. ` ,
-    MsgConsole: `Je suis prêt`
-}
