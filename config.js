545ee1ccb1de02d01aa89d46ffd7ef2a705ffe9a
TTRoyal <78960032+TTRoyal@users.noreply.github.com>
2021-02-12 12:39:12 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+//const config = require('./config.js')
+//const prefix = config.prefix
+module.exports = {
+    prefix: `.`,
+   
+    activity: `Mon prefix est. ` ,
+    MsgConsole: `Je suis prêt`
+}
